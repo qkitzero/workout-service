3a52af9692d86c69db47829469c7f4f7bb02b7bf
qkitzero <qkitzero@gmail.com>
2026-04-22 18:22:15 +0900

    add: ListSets endpoint (#3)

--- internal/application/set/usecase_test.go
+++ internal/application/set/usecase_test.go
@@ -9,6 +9,7 @@ import (
 
 	"go.uber.org/mock/gomock"
 
+	"github.com/qkitzero/workout-service/internal/domain/set"
 	mocksappauth "github.com/qkitzero/workout-service/mocks/application/auth"
 	mocks "github.com/qkitzero/workout-service/mocks/domain/set"
 )
@@ -51,6 +52,47 @@ func TestCreateSet(t *testing.T) {
 			setUsecase := NewSetUsecase(mockAuthService, mockSetRepository)
 
 			_, err := setUsecase.CreateSet(tt.ctx, tt.exercise, tt.rep, tt.weight, tt.trainedAt)
+			if tt.success && err != nil {
+				t.Errorf("expected no error, but got %v", err)
+			}
+			if !tt.success && err == nil {
+				t.Errorf("expected error, but got nil")
+			}
+		})
+	}
+}
+
+func TestListSets(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name            string
+		success         bool
+		ctx             context.Context
+		userID          string
+		verifyTokenErr  error
+		findByUserIDErr error
+	}{
+		{"success list sets", true, context.Background(), "fe8c2263-bbac-4bb9-a41d-b04f5afc4425", nil, nil},
+		{"failure verify token error", false, context.Background(), "", fmt.Errorf("verify token error"), nil},
+		{"failure empty user id", false, context.Background(), "", nil, nil},
+		{"failure find by user id error", false, context.Background(), "fe8c2263-bbac-4bb9-a41d-b04f5afc4425", nil, errors.New("find by user id error")},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			mockAuthService := mocksappauth.NewMockAuthService(ctrl)
+			mockSetRepository := mocks.NewMockSetRepository(ctrl)
+			mockAuthService.EXPECT().VerifyToken(tt.ctx).Return(tt.userID, tt.verifyTokenErr).AnyTimes()
+			mockSetRepository.EXPECT().FindByUserID(gomock.Any()).Return([]set.Set{}, tt.findByUserIDErr).AnyTimes()
+
+			setUsecase := NewSetUsecase(mockAuthService, mockSetRepository)
+
+			_, err := setUsecase.ListSets(tt.ctx)
 			if tt.success && err != nil {
 				t.Errorf("expected no error, but got %v", err)
 			}
